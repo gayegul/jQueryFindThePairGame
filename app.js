095e038d01b59e9e3ea4efdcbdf5187bcb0099e8
Gaye Bulut <gayegul@gmail.com>
2015-09-20 12:42:38 -0700

    fix bugs, add to do list

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 $(document).ready(function() {
   //get list of imgs from Instagram
   var imgs = ["./pics/IMG_2528.jpg", "./pics/IMG_2572.jpg", "./pics/IMG_3226.jpg", "./pics/IMG_3638.jpg", "./pics/IMG_5771.jpg", "./pics/IMG_3135.jpg", "./pics/IMG_5929.jpg", "./pics/IMG_5963.jpg", "./pics/IMG_5925.jpg", "./pics/IMG_5733.jpg"];
-
   var arrayOfBoxes = $('li').toArray();
-
   var randomBoxNumber = function() {
     return Math.floor(Math.random()* (arrayOfBoxes.length - 1));
   };
 
-  function assignPic() {
+  function assignImages() {
     for(var i = 0; i < imgs.length; i++) {
       var counter = 1;
       while(counter < 3) {
@@ -17,9 +15,7 @@ $(document).ready(function() {
         img.attr('src', imgs[i]);
         $(arrayOfBoxes[currentNumber]).find('a').append(img);
         img.hide();
-
         $(arrayOfBoxes[currentNumber]).click(handleClick);
-
         var index = arrayOfBoxes.indexOf(arrayOfBoxes[currentNumber]);
         if (index > -1) {
           arrayOfBoxes.splice(index, 1);
@@ -31,7 +27,7 @@ $(document).ready(function() {
     var openImgNumber = 0;
     function handleClick(e) {
       var liElement = $(e.target);
-
+      console.log('clicked');
       if(openImgNumber === 0) {
         handleFirstClick(liElement);
       }
@@ -61,12 +57,19 @@ $(document).ready(function() {
         firstLiElement.find('img').slideDown( 300 ).delay( 300 ).fadeIn( 400 ).fadeOut();
       }
       else {
-        firstLiElement.find('img').show().off();
-        liElement.find('img').show().off();
+        firstLiElement.off();
+        liElement.off();
+        firstLiElement.find('img').show();
+        liElement.find('img').show();
       }
       openImgNumber = 0;
     }
   }
 
-  assignPic();
+//reset button
+//show number of clicks
+//customize number of li's and don't put li's in html
+//get pics from Instagram API
+//find a scoreboard service to keep track of scores
+  assignImages();
 });
